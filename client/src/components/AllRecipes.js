e87c0b9012a79cf651757f044fd0ffe65c9c21b8
Jacosta von Achten <Markandjacosta@Jacostas-MacBook-Air.local>
2021-05-24 11:14:18 +0100

    reactstrap container sorted, sort of responsive but card layout needs more work re image size and title space

--- client/src/components/AllRecipes.js
+++ client/src/components/AllRecipes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button,  Card } from 'reactstrap';
+import { Container, Row, Col, Button,  Card } from 'reactstrap';
 import {CardImg,  CardTitle } from 'reactstrap';
 // onClick={() => getRecipe(data.id)} style={{ marginBottom: '1rem' }} 
 function AllRecipes(props){
@@ -12,9 +12,10 @@ function AllRecipes(props){
             {/* <h1>Click on a Jamie Oliver recipe to view the ingredients and cooking instructions</h1> */}
       
             { data ? 
-                <div className="row">
+                <Container>
+                <Row>
                     {data.map(r => (
-                        <div className="col-sm-6 col-md-4 col-lg-3 mb-3" key={r.id.toString()}>
+                        <Col lg="3" sm="4" xs="6" key={r.id.toString()}>
                             <Card> 
                             <CardTitle tag="h5">{r.name}</CardTitle> 
 
@@ -22,10 +23,11 @@ function AllRecipes(props){
 
                             <Button className="Button">Open Recipe</Button>
                             </Card>
-                        </div>
+                        </Col>
                     ))}
 
-                </div>
+                </Row>
+                </Container>
             : <h1>You have no recipes!</h1>
             }
                  
